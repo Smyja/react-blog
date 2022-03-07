dd9a9262bf42ac19d8e2eb9bf21d57f59019422b
Smyja <akposlive59@gmail.com>
2022-03-07 12:36:52 +0100

    set authenticated permission for create

--- backend/newsletter/permissions.py
+++ backend/newsletter/permissions.py
@@ -1,1 +1,7 @@
-from 
+from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
+
+class IsAuthor(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj.user:
+            return True
