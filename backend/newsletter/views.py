ce9f590e8401108495075223289f55c809a5459d
Smyja <akposlive59@gmail.com>
2022-06-30 18:38:24 +0100

    update

--- backend/newsletter/views.py
+++ backend/newsletter/views.py
@@ -5,6 +5,7 @@ from .models import Post
 from .serializers import PostSerializer,  PostCreateSerializer
 
 class PostListView(generics.ListAPIView):
+    ''' List all posts '''
     permission_classes = [AllowAny]
     queryset =Post.objects.all()
     serializer_class = PostSerializer
